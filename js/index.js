017154cc0da04070085eeaf9fcf0561854d356a5
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-08-31 12:22:36 +0900

    js修正

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-// var endPoint = 'https://nameless-sands-66548.herokuapp.com/instagram'
-var endPoint = 'http://localhost:9000/action.php'
+var endPoint = './action.php'
 var total = 0
 var progress = 0
 
@@ -7,19 +6,13 @@ var progress = 0
  * ぶっこ抜くボタン押下時 
  */
 $('#submit').click(function() {
-    console.log("osita")
-    initializeProgress()
     var titles = $('#titles').val().split('\n')
-    console.log(titles)
     total = titles.length
     exec(titles)
-    // titles.forEach(function(title) { exec([title]) })
 })
 
 function exec(titles) {
-    console.log("ikimasu")
     $('#result-view-title').text('取得中...')
-    $('#result-view-progress').text(`${progress}/${total}`)
     var data ={
         'titles' : titles,
     }
@@ -34,28 +27,18 @@ function exec(titles) {
         data:JSON.stringify(data)
     })
     .done((results) => {
-        console.log(results)
         var resultText = ''
         results.forEach(function(result) {
             let title = result.title
             let authors = result.authors
             resultText += title + '\t' + authors + '\n'
         })
-        console.log(resultText)
         $('#resultText').val(resultText);
     })
     .fail((data) => {
-        console.log("ajax失敗")
     })
     .always((data) => {
-        initializeProgress()
+        $('#result-view-title').text('結果')
     });
-}
-
-function initializeProgress() {
-    total = 0
-    progress = 0
-    $('#result-view-progress').text('')
-    $('#result-view').empty()
 }
 
